7e09d6a047c0e4916d701d1461ac0ebb62b25f8e
Lewis Allen <lewis.f.allen.mil@army.mil>
2025-09-05 09:39:37 -0500

    initial commit

---
+++ aircraft_springboot/src/main/java/swf/army/mil/aircraft_springboot/AircraftSpringbootApplication.java
@@ -0,0 +1,13 @@
+package swf.army.mil.aircraft_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AircraftSpringbootApplication.class, args);
+    }
+
+}
---
+++ aircraft_springboot/src/test/java/swf/army/mil/aircraft_springboot/AircraftSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package swf.army.mil.aircraft_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
